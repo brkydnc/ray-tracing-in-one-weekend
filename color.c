3f9f9946d7eb1c0f9030ba09fb39dce2e372cd62
Berkay Dinç <berkayydinc@gmail.com>
2021-02-23 12:12:53 +0300

    Make colors range from 0 to 1

--- color.c
+++ color.c
@@ -4,8 +4,8 @@
 #include "sphere.h"
 #include "util.h"
 
-const Color WHITE = {255.0, 255.0, 255.0};
-const Color SKY_BLUE = {128.0, 179.0, 255.0};
+const Color WHITE = {1.0, 1.0, 1.0};
+const Color SKY_BLUE = {0.5, 0.7, 1.0};
 
 Color color_from(double r, double g, double b) {
   Color color = {r, g, b};
@@ -13,19 +13,19 @@ Color color_from(double r, double g, double b) {
 }
 
 void write_color(FILE *out, Color color, int samples_per_pixel) {
-  unsigned red = (unsigned) color.x;
-  unsigned green = (unsigned) color.y;
-  unsigned blue = (unsigned)  color.z;
+  double red = color.x;
+  double green = color.y;
+  double blue = color.z;
 
   double scale = 1.0 / samples_per_pixel;
   // gamma correction & scale to average color
-  red = sqrt(red * scale / 255) * 255;
-  green = sqrt(green * scale / 255) * 255;
-  blue = sqrt(blue * scale / 255) * 255;
+  red = sqrt(red * scale);
+  green = sqrt(green * scale);
+  blue = sqrt(blue * scale);
 
-  red = clamp(red, 0.0, 255.0);
-  green = clamp(green, 0.0, 255.0);
-  blue = clamp(blue, 0.0, 255.0);
+  red = clamp(red, 0.0, 1.0) * 255;
+  green = clamp(green, 0.0, 1.0) * 255;
+  blue = clamp(blue, 0.0, 1.0) * 255;
 
-  fprintf(out, "%u %u %u\n", red, green, blue);
+  fprintf(out, "%i %i %i\n", (int) red, (int) green, (int) blue);
 }
